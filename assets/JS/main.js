040009908a02d19d0e090bb20a94d2a82275e9a9
BetoSandoval <alberto.sa1391@gmail.com>
2020-07-30 19:10:39 -0500

    silder funcionando

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -1,3 +1,4 @@
+// Menu interactivo desplegable
 let boton = document.getElementById('icono');
 let enlaces = document.getElementById('enlaces');
 let contador = 0;
@@ -12,3 +13,32 @@ boton.addEventListener('click', function(){
          contador = 0;
     }
 })
+
+// Slider 
+let slider = document.querySelector('.slider-contenedor');
+let sliderIndividual = document.querySelectorAll('.contenido-slider');
+let cuenta = 1;
+let width = sliderIndividual[0].clientWidth;
+let intervalo = 5000;
+
+window.addEventListener('resize', function(){
+    width = sliderIndividual[0].clientWidth;
+})
+
+setInterval(function(){
+    slides();
+}, intervalo);
+
+function slides(){
+    slider.style.transform = "translate("+(-width*cuenta)+"px)";
+    slider.style.transition = 'transform 3s';
+    cuenta++;
+
+    if(cuenta == sliderIndividual.length){
+        setTimeout(function(){
+            slider.style.transform = 'translate(0px)';
+            slider.style.transition = 'transform 0s';
+            cuenta=1;
+        }, 5000);
+    }
+};
